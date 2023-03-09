cb2ecef66294115b4771c4f250a41e78abdbf7f5
Aaron Bush <81124911+Ambush3@users.noreply.github.com>
2023-03-08 20:46:57 -0500

    Update secret_santa.py

--- secret_santa.py
+++ secret_santa.py
@@ -1,13 +1,10 @@
-#Import Libraries
 import re
 import random
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-sender_address = 'email'
-sender_pass = 'password'
 
 names = []
 emails = []
@@ -24,43 +21,40 @@ How would you like to enter the information?
 
 # info entry method choice
 x = 0
-while x == 0:
+while (x == 0):
     try:
         option = int(input("Info entry method (1 or 2): "))
-        if option > 2 or option < 1:
+        if (option > 2 or option < 1):
             print("ERROR: You can only input 1 or 2!")
-            print("""HOW would you like to enter the information? 
-                    1. give a csv (.csv) file with format of:
-                         name, email address
-                     2. manually enter information """)
+            print("""How would you like to enter the information?
+                  1. give a text (.txt) file
+                  2. manually enter information""")
         else:
             x = 1
-
     except ValueError:
-        print("ERROR: Please input 1 or 2")
-        print("""HOW would you like to enter the information? 
-        1. give a text (.txt) file with format of:
-             name, email address
-         2. manually enter information """)
+        print("ERROR: Please input 1 or 2!")
+        print("""How would you like to enter the information?
+        1. give a text (.txt) file
+        2. manually enter information""")
 
 # number of participants
 x = 0
-while x == 0:
+while (x == 0):
     try:
         count = int(input("Enter number of participants: "))
-        if count < 2:
+        if (count < 2):
             print("ERROR: Number of participants must be 2 or more!")
         else:
             x = 1
     except ValueError:
         print("ERROR: Please input a valid integer number!")
 
-# option 1: read the file
-if option == 1:
+# option 1: read the file (assumes file format is correct)
+if (option == 1):
     x = 0
-    while x == 0:
-        filename = str(input("Name of csv file (must end in .csv): "))
-        if filename[-4:] == '.csv':
+    while (x == 0):
+        filename = str(input("Name of text file (must end in .txt): "))
+        if (filename[-4:] == '.txt'):
             x = 1
         else:
             print("ERROR: Please input a file name which ends with .txt")
@@ -72,18 +66,18 @@ if option == 1:
 
 
 # option 2: manually get info
-elif option == 2:
+elif (option == 2):
 
     regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 
-    print("Input the participants' information!")
+    print("Ok! It's time for you to input the participants' information!")
     for i in range(1, count + 1):
-        name = str(input(f'Name of participant {i}: '))
+        name = str(input(f'Enter participant the name of participant {i}: '))
         names.append(name)
         x = 0
-        while x == 0:
+        while (x == 0):
             email = str(input(f'Enter the email of participant {i}: '))
-            if re.search(regex, email):
+            if (re.search(regex, email)):
                 emails.append(email)
                 x = 1
             else:
@@ -93,7 +87,7 @@ possible_santa = names.copy()
 
 cont = 0
 
-while cont == 0:
+while (cont == 0):
 
     redo = False
 
@@ -102,18 +96,89 @@ while cont == 0:
     for i in range(0, len(names)):
         recip = random.randint(0, len(possible_santa) - 1)
         x = 0
-        while x == 0:
-            if names[i] == possible_santa[recip]:
-                if len(possible_santa) == 1:
+        while (x == 0):
+            if (names[i] == possible_santa[recip]):
+                if (len(possible_santa) == 1):
                     redo = True
                     x = 1
                 else:
                     recip = random.randint(0, len(possible_santa) - 1)
             else:
                 x = 1
-        if not redo:
+        if (redo != True):
             recipient.append(possible_santa[recip])
             possible_santa.pop(recip)
             cont = 1
         else:
             cont = 0
+
+# Sending the emails to the participants
+
+import smtplib
+
+import random
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# this code must run for each name
+for i in range(0, count):
+    # the message which will be sent in the email
+    mail_content = f'''Hello {names[i]},
+
+You are the secret santa of {recipient[i]}!
+
+Remember the budget is ${budget}
+    '''
+
+    # sender address
+    sender_address= os.getenv("EMAIL")
+    sender_pass= os.getenv("PASSWORD")
+
+    # sets the email address the email will be sent to
+    receiver_address = emails[i]
+
+    # sets up the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_address  # your email address
+    message['To'] = receiver_address  # Secret Santa's email address
+    message['Subject'] = 'Secret Santa'  # subject of the
+
+    # sets the body of the mail
+    message.attach(MIMEText(mail_content, 'plain'))
+
+    # creates the SMTP session for sending the mail (gmail)
+    # session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
+    # session.starttls()  # enable security
+    # session.login(sender_address, sender_pass)  # login with mail_id and password
+    # text = message.as_string()
+    # session.sendmail(sender_address, receiver_address, text)
+    # session.quit()
+
+    # creates the SMTP session for sending the mail (hotmail)
+    # session = smtplib.SMTP('smtp.live.com', 587)  # use Hotmail with port 587
+    # session.starttls()  # enable security
+    # session.login(sender_address, sender_pass)  # login with mail_id and password
+    # text = message.as_string()
+    # session.sendmail(sender_address, receiver_address, text)
+    # session.quit()
+
+    # creates the SMTP session for sending the mail (yahoo)
+    session = smtplib.SMTP('smtp.mail.yahoo.com', 587)  # use yahoo with port 58
+    session.starttls()  # enable security
+    session.login(sender_address, sender_pass)  # login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+
+
+
+    print('Mail Sent')
+
+# Writing the allocations to a file
+allocations = open("SantaAllocations.txt", "w+")
+
+for i in range(0, len(names)):
+    allocations.write(f'{names[i]} is the secret santa of {recipient[i]}\n')
+
+allocations.close()
